351329620e97fe04d0cee8a6c94d974dcd6d6ca2
Joerg Boehmer <joerg.boehmer@snafu.de>
2024-05-18 18:58:16 +0200

    change samples

--- generate_samples_symbol_lib.sh
+++ generate_samples_symbol_lib.sh
@@ -2,4 +2,4 @@
 
 rm -f samples.kicad_sym
 
-./csv_to_kicad.py -o samples samples/simple_symbols.csv samples/alternative_pin_functions.csv samples/bus.csv samples/derived_symbols.csv
+./csv_to_kicad.py -o samples samples/*.csv
